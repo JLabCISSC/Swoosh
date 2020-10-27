b5a3b9da8a438e9b6b13a2ad2871322d2064dc6a
JLab <jlabcissc@gmail.com>
2020-10-27 15:22:08 -0400

    finished the refactor

--- app/src/main/java/com/jarradl/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/jarradl/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.jarradl.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.jarradl.swoosh.Model.Player
@@ -13,9 +14,21 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("","")
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
     }
 
     fun onMensClicked(view: View) {
